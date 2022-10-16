0b22d8f59394bd05b53f5d30acd0ddc23e8aa492
Stephanie Smith <stephiesmith@gmail.com>
2022-10-16 10:26:26 -0700

    making printouts more legible, fixing issues resetting default time scales

--- cicadas-installation/cicadas-installation.ino
+++ cicadas-installation/cicadas-installation.ino
@@ -10,23 +10,18 @@
     - TODO: randomize base PWM drastically (each cycle, not each ramp) - strong brood vs weaker brood
     - a lifecycle is all of the motors doing this for a period of time
     - one ball per that is extra noisy? (the last one?)
-  - TODO: main board button - trigger lifecycle button
+
   - TODO: main board switch - toggle between using programmed cycle vs waiting for controller inputs, or toggle between knob modes
   - main board intensity limit knob
   - main board time scale knob
   - TODO: peripheral switch for knobs - use knobs to set intensity limit per motor. (how to save state though?)
   - TODO: peripheral switch for switches - override with motor switches (everything else is preprogrammed)
-  - From controller: (serial print values to get sense)
-    - main board switch to enable controller (nice to have)
-    - realtime change sleeptime - ctrlKnobValue[0]
-    - realtime change awaketime - ctrlKnobValue[1]
-    - realtime change ramp time - ctrlKnobValue[2]
-    - TODO: do we need intensity min? two knobs to control threshold?
-  - TODOs immediate prep
-    - need ability to easily change if brood starts asleep or awake (in the code)
+  - TODOs for installation prep
+    - DONE - need ability to easily change if brood starts asleep or awake (in the code)
     - random event, modulo 6 or 7 (rand) that ramps motors up and down in unison to full pwm
-    - TEST - serial print scale variables to easily set these during the install
-    - TEST - need ability to toggle knob inputs for changing timing settings (use hard-coded values vs use knobs) - use onboard switch (on = use knob vals)
+    - DONE - serial print scale variables to easily set these during the install
+    - DONE - need ability to toggle knob inputs for changing timing settings (use hard-coded values vs use knobs) - use onboard switch (on = use knob vals)
+    - main board button - trigger lifecycle button -- button sets awake to true?
 
 **************************************/
 
@@ -36,20 +31,24 @@
 /* VARS & CLASSES -------------------------------------------- */
 
 // ---- Limits, Constants, States ---- //
-// if brood is awake or sleeping
+// if brood is awake or sleeping - also sets starting state
 bool awake = false;
 // sleep duration (s)
-unsigned long sleepTime = 6; // 6
+unsigned long defaultSleepTime = 10; // 6
+unsigned long sleepTime = defaultSleepTime;
 // awake duration  (s) 
-unsigned long awakeTime = 24; // 24
+unsigned long defaultAwakeTime = 24; // 24
+unsigned long awakeTime = defaultAwakeTime;
 // time scale (ms)
-long timeScale = 1000; // 1000
+long defaultTimeScale = 1000; // 1000
+long timeScale = defaultTimeScale;
 // min timeScale (ms)- double time;
 int timeScaleMin = 500;
 // max timeScale (ms) - half time;
 int timeScaleMax = 2000;
 // base ramp time for motors (ms) TODO: change to (s)?
-unsigned long rampBasis = 6000; // 6000
+unsigned long defaultRampBasis = 6000; // 6000
+unsigned long rampBasis = defaultRampBasis;
 // min motor ramp time (s)
 float motorRampMin = 0.5;
 // max motor ramp time (s)
@@ -65,36 +64,36 @@ int intensityMax = 255;
 // number of motors in arrays
 static const uint8_t motorCount = 12;
 // motor pins
-RBD::Motor motor[] = {(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13)};
+RBD::Motor motor[12] = {(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13)};
 // ramp direction for each per motor
-bool rampingUp[] = {true, true, true, true, true, true, true, true, true, true, true, true}; 
+bool rampingUp[12] = {true, true, true, true, true, true, true, true, true, true, true, true}; 
 // ramp durations for each motor (ms)
-unsigned long rampTime[] = {rampBasis, rampBasis, rampBasis, rampBasis, rampBasis, rampBasis, rampBasis, rampBasis, rampBasis, rampBasis, rampBasis, rampBasis};
+unsigned long rampTime[12] = {rampBasis, rampBasis, rampBasis, rampBasis, rampBasis, rampBasis, rampBasis, rampBasis, rampBasis, rampBasis, rampBasis, rampBasis};
 // create and set timer for each motor
-RBD::Timer rampTimer[] = {rampTime[0], rampTime[1], rampTime[2], rampTime[3], rampTime[4], rampTime[5], rampTime[6], rampTime[7], rampTime[8], rampTime[9], rampTime[10], rampTime[11]};
+RBD::Timer rampTimer[12] = {rampTime[0], rampTime[1], rampTime[2], rampTime[3], rampTime[4], rampTime[5], rampTime[6], rampTime[7], rampTime[8], rampTime[9], rampTime[10], rampTime[11]};
 
 // ---- Motor Control Inputs ---- //
-static const uint8_t ctrlSwitchPin[] = {22,23,24,25,26,27,28,29,30,31,32,33};
-static const uint8_t ctrlKnobPin[] = {A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11};
+static const uint8_t ctrlSwitchPin[12] = {22,23,24,25,26,27,28,29,30,31,32,33};
+static const uint8_t ctrlKnobPin[12] = {A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11};
 
 // ---- General Switches ---- //
 // switchPin[0] = main board button
 // switchPin[1] = main board switch
 // switchPin[2] = peripheral switch for switches (override motor on/off) 
 // switchPin[3] = peripheral swich for knobs (calibration)
-static const uint8_t switchPin[] = {34,35,36,37};
+static const uint8_t switchPin[4] = {34,35,36,37};
 
 // ---- General Knobs ---- //
 // knobPin[0] = main board intensity knob
 // knobPin[1] = main board time scale knob
 // knobPin[2] = peripheral j-connector input
-static const uint8_t knobPin[] = {A12,A13,A14};
+static const uint8_t knobPin[3] = {A12,A13,A14};
 
 // ---- Reading / Storing Values ---- //
-int ctrlKnobValue[] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
-int ctrlSwitchValue[] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
-int knobValue[] = {-1,-1,-1};
-int switchValue[] = {-1,-1,-1,-1};
+int ctrlKnobValue[12] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+int ctrlSwitchValue[12] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+int switchValue[4] = {-1,-1,-1,-1};
+int knobValue[3] = {-1,-1,-1};
 
 // ---- Global Timers ---- //
 // sleep timer
@@ -146,12 +145,12 @@ void ramp(int i, bool rampUp = false) {
     } else {
       // TODO: explore making the ramp timer shorter for the sustain period
       motor[i].ramp(motorSpeed(), rampTime[i]);
-      // Serial.print("Sustain PWM for [");
-      // Serial.print(i);
-      // Serial.print("]: ");
-      // Serial.print(variedIntensity());
-      // Serial.print(" for ");
-      // Serial.println(rampTime[i]);
+       Serial.print("-- sustain for [");
+       Serial.print(i);
+       Serial.print("]: ");
+       Serial.print(variedIntensity());
+       Serial.print(" for ");
+       Serial.println(rampTime[i]);
     }
   } else {
     // off
@@ -167,7 +166,7 @@ void setup() {
   randomSeed(analogRead(A15));
 
   // setup digital pins
-  for (int i = 0; i < 12; i++) {
+  for (int i = 0; i < motorCount; i++) {
     pinMode(ctrlSwitchPin[i], INPUT);
   }
   for (int i = 0; i < 4; i++) {
@@ -205,8 +204,13 @@ void setup() {
   Serial.println(rampBasis);
 
   // start asleep
-  sleepTimer.setTimeout(sleepTime * timeScale);
-  sleepTimer.restart();
+  if (!awake) {
+    sleepTimer.setTimeout(sleepTime * timeScale);
+    sleepTimer.restart();
+  } else {
+    awakeTimer.setTimeout(awakeTime * timeScale);
+    awakeTimer.restart();
+  }
 }
 
 /* LOOP ----------------------------------------------------- */
@@ -216,10 +220,6 @@ void loop() {
   // read knobPin
   for (int i = 0; i < 3; i++) {
     knobValue[i] = analogRead(knobPin[i]);
-    // Serial.print("knobValue[");
-    // Serial.print(i);
-    // Serial.print("] = ");
-    // Serial.println(knobValue[i]);
   }
   // read switchPin
   for (int i = 0; i < 4; i++) {
@@ -242,6 +242,11 @@ void loop() {
     sleepTime = map(ctrlKnobValue[0], 0, 1023, 6, 180); // 3 minutes;
     awakeTime = map(ctrlKnobValue[1], 0, 1023, 24, 180); // 3 minutes;
     rampBasis = map(ctrlKnobValue[2], 0, 1023, 500, 20000); // half second to 20 seconds;
+  } else {
+    timeScale = defaultTimeScale;
+    sleepTime = defaultSleepTime;
+    awakeTime = defaultAwakeTime; // 3 minutes;
+    rampBasis = defaultRampBasis; // half second to 20 seconds;
   }
 
   if(sleepTimer.onExpired()) {
@@ -249,16 +254,17 @@ void loop() {
     awakeTimer.setTimeout(awakeTime * timeScale);
     awakeTimer.restart();
 
-    Serial.print("Waking for ");
-    Serial.println(awakeTime * timeScale);
+    Serial.print("WAKING for ");
+    Serial.print(float(awakeTime) * float(timeScale) * .000016);
+    Serial.println(" (min) ----------------- // ");
     // output scale for installation reference
-    Serial.print("timeScale: ");
+    Serial.print("- timeScale: ");
     Serial.println(timeScale);
-    Serial.print("sleepTime: ");
+    Serial.print("- sleepTime: ");
     Serial.println(sleepTime);
-    Serial.print("awakeTime: ");
+    Serial.print("- awakeTime: ");
     Serial.println(awakeTime);
-    Serial.print("rampBasis: ");
+    Serial.print("- rampBasis: ");
     Serial.println(rampBasis);
   }
 
@@ -284,16 +290,17 @@ void loop() {
     sleepTimer.setTimeout(sleepTime * timeScale);
     sleepTimer.restart();
 
-    Serial.print("Sleeping for ");
-    Serial.println(sleepTime * timeScale);
+    Serial.print("SLEEPING for ");
+    Serial.print(float(sleepTime) * float(timeScale) * .000016);
+    Serial.println(" (min) --------------- // ");
     // output scale for installation reference
-    Serial.print("timeScale: ");
+    Serial.print("- timeScale: ");
     Serial.println(timeScale);
-    Serial.print("sleepTime: ");
+    Serial.print("- sleepTime: ");
     Serial.println(sleepTime);
-    Serial.print("awakeTime: ");
+    Serial.print("- awakeTime: ");
     Serial.println(awakeTime);
-    Serial.print("rampBasis: ");
+    Serial.print("- rampBasis: ");
     Serial.println(rampBasis);
   }
 
